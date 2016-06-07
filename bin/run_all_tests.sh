52b4b91fe82b781e6ada20d391c4e089dec00b9a
Adam Keating <adamkeating@me.com>
2016-06-07 08:45:36 -0700

    Test for existing e2e server

--- bin/run_all_tests.sh
+++ bin/run_all_tests.sh
@@ -1,23 +1,28 @@
 #!/usr/bin/env bash
+function server_pid {
+  echo $(lsof -i:4002 | grep beam | awk '{ print $2 }')
+}
 
 set -e
 npm run test-server
 npm run test-client-unit
 
-# Let's wait for the server to start
-exec 3< <(MIX_ENV=teste2e mix s &)
-while read line; do
-   case "$line" in
-   *Running*)
-      break
-      ;;
-   *)
-      ;;
-   esac
-done <&3
-exec 3<&-
+# Recovering from an arror; is there a server running?
+if [ -n $(server_pid) ]; then
+  # Let's wait for the server to start
+  exec 3< <(MIX_ENV=teste2e mix s &)
+  while read line; do
+     case "$line" in
+     *Running*)
+        break
+        ;;
+     *)
+        ;;
+     esac
+  done <&3
+  exec 3<&-
+fi
 
 npm run test-e2e
 
-# Find the pid of the running server, then kill it
-kill $(lsof -i:4002 | grep beam | awk '{ print $2 }')
+kill $(server_pid)
