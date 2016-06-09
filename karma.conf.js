6e70459f501b7afa80e5ab603f94a382d3dcbca3
Adam Keating <adamkeating@me.com>
2016-06-09 08:43:51 -0700

    Karma log info instead of debug

--- karma.conf.js
+++ karma.conf.js
@@ -9,7 +9,7 @@ module.exports = function(config) {
     reporters: ['progress'],
     port: 9876,
     colors: true,
-    logLevel: config.LOG_DEBUG,
+    logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
